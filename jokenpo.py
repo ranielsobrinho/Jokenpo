1211f80bc3ec05c9f1e875bb8846d919d833691d
Raniel <raniel25sobrinho@gmail.com>
2020-06-05 16:35:48 -0300

    Usando funções no código para ficar um pouco mais organizado e fazendo uma limpeza nos códigos desnecessários

--- jokenpo.py
+++ jokenpo.py
@@ -1,5 +1,5 @@
 #importando módulos
-from  random import randint
+from  random import choice
 from time import sleep
 
 #uma breve conversa antes do jogo começar
@@ -10,19 +10,29 @@ if resposta == 'NÃO' or resposta == 'NAO':
     print("Que pena! Então até a proxima.")
 else:
     print("Ótimo, vamos jogar pedra, papel e tesoura!")
-#definindo os itens com emoji e criando um loop para o jogo
+    sleep(2)
     while True:
+        def empate():
+            if maquina == jogador:
+                print("DEU EMPATE!")
+
+        def maquinaWins():
+            if maquina == "pedra" and jogador == "tesoura" or maquina == "papel" and jogador == "pedra" or maquina == "tesoura" and jogador == "papel":
+                print("EU VENCI!")
+
+        def jogadorWins():
+            if maquina == "pedra" and jogador == "papel" or maquina == "papel" and jogador == "tesoura" or maquina == "tesoura" and jogador == "pedra":
+                print("VOCÊ GANHOU!")
 
         itens = ("pedra", "papel", "tesoura")
-        maquina = randint(0, 2)
+        maquina = choice(itens)
         print('''Escolha uma das opções:
-        [ 0 ] PEDRA
-        [ 1 ] PAPEL
-        [ 2 ] TESOURA''')
-        jogador = int(input("Faça a sua jogada: "))
-        if jogador >= 3:
-            print("Não existe essa opção, por favor vamos brincar direito!")
-            break
+-pedra
+-papel
+-tesoura''')
+        jogador = str(input("Escreva a sua opção: "))
+        while jogador != "pedra" and jogador != "papel" and jogador != "tesoura":
+            jogador = str(input("Escreva a sua opção: "))
         #fazendo a simulação como se estivesse falando real
         print("JO")
         sleep(1)
@@ -31,33 +41,13 @@ else:
         print("PO!")
         #mostra a escolha da máquina e do jogador
         print('-=' * 11)
-        print("Eu escolho {}".format(itens[maquina]))
-        print("{} jogou {}".format(nome, itens[jogador]))
+        print("Eu escolho {}".format(maquina))
+        print("{} jogou {}".format(nome, jogador))
         print('-=' * 11 )
         #mostra quem ganhou ou se deu empate
-        if maquina == 0: #PEDRA
-            if jogador == 0:
-                print("EMPATE!")
-            elif jogador == 1:
-                print("{} VENCEU!".format(nome))
-            elif jogador == 2:
-                print("EU VENCI!")
-        elif maquina == 1:  #PAPEL
-            if jogador == 0:
-                print("EU VENCI!")
-            elif jogador == 1:
-                print("EMPATE!")
-            elif jogador == 2:
-                print("{} VENCEU!".format(nome))
-        elif maquina == 2:  #TESOURA
-            if jogador == 0:
-                print("{} VENCEU!".format(nome))
-            elif jogador == 1:
-                print("EU VENCI!")
-            elif jogador == 2:
-                print("EMPATE!")
-        else:
-            print("OPÇÃO INVÁLIDA! TENTE NOVAMENTE.")
+        empate()
+        maquinaWins()
+        jogadorWins()       
         #finalizando o jogo ou recomeçando o
         opcao = input("Pressione \'q\' para sair ou digite qualquer letra para jogar: ")
         if opcao == 'Q' or opcao == 'q':
